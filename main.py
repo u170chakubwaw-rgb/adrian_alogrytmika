fbe44abd64d32915b47bad82d973844df97ec34b
Kuba Chacinski <u170_chakub_waw@technischools.com>
2026-09-09 12:19:57 +0200

    cos tam

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print("Dzień dobry świecie")
+import sqlite3
+
+con = sqlite3.connect("bank.db")
+cursor = con.cursor()
+
+cursor.execute("""
+    CREATE TABLE users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT
+        name VARCHAR(20) NOT NULL,
+        surname VARGCHAR(30) UNIQUE NOT NULL,
+        phone_number VARCHAR(12) UNIQUE NOT NULL,
+    )""")
+
+cursor.execute("""CREATE TABLE transactions(
+                                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                        amount DECIMAL(10, 2)
+                                        from_uid INTEGER NOT NULL,
+                                        to_uid INTEGER NOT NULL,
+               
+                                        FORGEIN KEY (from_uid) REFERENCES users(id)
+                        
+               
+               
+               
+               )""")
